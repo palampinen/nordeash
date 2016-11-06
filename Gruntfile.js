8e56cbd3f33022a1168aa212cb63ba1d7a527f9f
thedpr <palampinen@gmail.com>
2016-11-06 22:08:47 +0200

    add partial filepath to build config

--- Gruntfile.js
+++ Gruntfile.js
@@ -337,7 +337,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/{,*/}*.html'],
+          src: ['*.html', 'views/{,*/}*.html', 'partials/{,*/}*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
@@ -376,6 +376,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
+            'partials/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
           ]
